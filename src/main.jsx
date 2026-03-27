fd7de42cc9b9617d5486b2644178e3b32b8f6b5a
ShawnGilbert <tecnoblix@gmail.com>
2026-03-27 13:16:04 -0400

    switch from smoketest to real app

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-function SmokeTest() {
-  return (
-    <div style={{ padding: "40px", color: "white", fontFamily: "Arial, sans-serif" }}>
-      <h1>Smoke test works</h1>
-      <p>If you can see this, Vite + React + index.html are fine.</p>
-    </div>
-  );
-}
+import App from "./App.jsx";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <SmokeTest />
+    <App />
   </React.StrictMode>
 );
