2acfb8a4d9e551ee0e4b656260d537bddedb1432
ShawnGilbert <tecnoblix@gmail.com>
2026-03-29 09:35:43 -0400

    Put sourcemap under build in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    sourcemap: false,   // 👈 key fix
-  },
-})
+plugins: [react()],
+build: {
+sourcemap: false,
+rollupOptions: {
+output: {
+sourcemap: false,
+},
+},
+},
+});
